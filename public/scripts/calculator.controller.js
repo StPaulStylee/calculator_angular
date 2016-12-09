cb3ea5a15bfba248f63f9dba3098630822bcd3dd
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2016-12-09 11:41:33 -0600

    added some functional value buttons

--- public/scripts/calculator.controller.js
+++ public/scripts/calculator.controller.js
@@ -6,4 +6,11 @@ function CalculatorController(CalculatorService) {
   console.log('GameController Loaded');
   var ctrl = this;
 
+  ctrl.valueA = "";
+
+  ctrl.inputValue = function(value) {
+      console.log(value);
+      ctrl.valueA += value;
+      console.log(ctrl.valueA);
+  }
 } //End of CalculatorController
