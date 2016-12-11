5f8e41c5695d1991bdc6c4cc0dd2ea3aa849ae3d
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2016-12-10 20:49:45 -0600

    operators and values ready for submission

--- public/scripts/calculator.controller.js
+++ public/scripts/calculator.controller.js
@@ -6,11 +6,55 @@ function CalculatorController(CalculatorService) {
   console.log('GameController Loaded');
   var ctrl = this;
 
-  ctrl.valueA = "";
+  ctrl.operatorChosen = false;
+  ctrl.valueDisplay = '';
+  ctrl.valuesToCalculate = {valueA: '', valueB: '', operator: ''};
 
   ctrl.inputValue = function(value) {
+    if (ctrl.operatorChosen = false) {
       console.log(value);
-      ctrl.valueA += value;
-      console.log(ctrl.valueA);
+      ctrl.valueDisplay += value
+      console.log(ctrl.valueDisplay);
+    } else {
+      console.log(value);
+      ctrl.valueDisplay += value;
+      console.log(ctrl.valueDisplay);
+    }
+  }
+
+  ctrl.add = function(operator) {
+    ctrl.valuesToCalculate.valueA = parseInt(ctrl.valueDisplay);
+    ctrl.operatorChosen = true;
+    ctrl.valuesToCalculate.operator = operator;
+    ctrl.valueDisplay = '';
+    console.log('Add clicked');
+    console.log(ctrl.valuesToCalculate);
+  }
+
+  ctrl.subtract = function(operator) {
+    ctrl.valuesToCalculate.valueA = parseInt(ctrl.valueDisplay);
+    ctrl.operatorChosen = true;
+    ctrl.valuesToCalculate.operator = operator;
+    ctrl.valueDisplay = '';
+    console.log('subtract clicked');
+    console.log(ctrl.valuesToCalculate);
+  }
+
+  ctrl.multiply = function(operator) {
+    ctrl.valuesToCalculate.valueA = parseInt(ctrl.valueDisplay);
+    ctrl.operatorChosen = true;
+    ctrl.valuesToCalculate.operator = operator;
+    ctrl.valueDisplay = '';
+    console.log('multiply clicked');
+    console.log(ctrl.valuesToCalculate);
+  }
+
+  ctrl.divide = function(operator) {
+    ctrl.valuesToCalculate.valueA = parseInt(ctrl.valueDisplay);
+    ctrl.operatorChosen = true;
+    ctrl.valuesToCalculate.operator = operator;
+    ctrl.valueDisplay = '';
+    console.log('Divide clicked');
+    console.log(ctrl.valuesToCalculate);
   }
 } //End of CalculatorController
